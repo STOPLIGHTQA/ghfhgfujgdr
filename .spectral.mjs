88f982353a07a9d7c538e4d005c5eb7ad10b44e2
yashikacom <yashika.bajaj@smartbear.com>
2026-03-06 17:53:42 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjc0MTMxMA/spectral.js?branch_slug=main&token=7582649b-3e07-4c3b-8268-d129ddde3191";
+export default { extends: ruleset };
